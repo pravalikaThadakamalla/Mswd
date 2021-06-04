f3e076357d753618fee34ae4589954dba8bde0cb
pravalikaThadakamalla <74005412+pravalikaThadakamalla@users.noreply.github.com>
2021-06-04 10:48:49 +0530

    Add files via upload

---
+++ part2/countries/Country.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Country = ({ country, weather }) => {
+  return (
+    <div>
+      <h1><span>{country.name}</span></h1>
+      <p>capital {country.capital}<br />populations {country.population}</p>
+      <h2>languages</h2>
+      <ul>
+        {country.languages.map(language =>
+          <li key={language.name}>{language.name}<br /></li>
+        )}
+      </ul>
+      <p>
+      <img
+        src={country.flag}
+        alt={`Flag of ${country.name}`} 
+        height="70" 
+        width="80" 
+      />
+      </p>
+      <h2>Weather in {country.capital}</h2>
+      <p><b>temperature: </b> {weather['current'].temperature} Celcius</p>
+      <img src={weather['current'].weather_icons[0]} alt='weather icon' />
+      <p><b>wind: </b> {weather['current'].wind_speed} kph direction {weather['current'].wind_dir}</p>
+    </div>
+  )
+}
+
+export default Country
